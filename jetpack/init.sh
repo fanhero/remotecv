f4180349963f3164972e54acfddecb0eaeff046f
Christian Green <christian@fanhero.com>
2016-09-09 17:22:26 -0400

    Configuring prepack

---
+++ jetpack/init.sh
@@ -0,0 +1,5 @@
+# vim: fileencoding=utf-8 tw=100 noexpandtab ts=8 sw=8 ft=sh :
+
+source "$JETPACK_ROOT/lib/utils/output.sh"
+source "$JETPACK_ROOT/lib/utils/require.sh"
+source "$JETPACK_ROOT/lib/utils/fs.sh"
---
+++ jetpack/lib/required/pip.sh
@@ -0,0 +1,14 @@
+# vim: fileencoding=utf-8 tw=100 noexpandtab ts=8 sw=8 ft=sh :
+
+function install_pip {
+	shout "Installing Pip dependencies"
+
+	echo "Installing pip" | indent
+	curl -s https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python | indent
+
+	echo "Installing dependencies" | indent
+	PIP_SRC="$APP_DIR/.jetpack/python/src"
+	mkdir -p "$PIP_SRC"
+
+	pip install -r "$BUILD_DIR/requirements.txt" --exists-action=w --src="$PIP_SRC" | indent
+}
