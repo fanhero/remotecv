2d9a31158f9266fe915b23749c7d09d060ea7f35
Christian Green <christian@fanhero.com>
2016-09-09 17:39:01 -0400

    removed jetpack

--- jetpack/lib/required/npm-global.sh
+++
@@ -1,7 +0,0 @@
-# vim: fileencoding=utf-8 tw=100 noexpandtab ts=8 sw=8 ft=sh :
-
-function install_npm-global {
-	shout "Installing a global NPM package"
-
-	npm -g install "$1" | indent
-}
--- jetpack/lib/utils/fs.sh
+++
@@ -1,5 +0,0 @@
-# vim: fileencoding=utf-8 tw=100 noexpandtab ts=8 sw=8 ft=sh :
-
-function cp_contents {
-	tar -C "$1" -c . | tar -C "$2" -x
-}
