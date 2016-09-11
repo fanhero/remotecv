466ef15d0a116481b9907ecef5bb68e26fc92004
Christian Green <christian@fanhero.com>
2016-09-10 20:06:29 -0400

    Added variable s3 support

---
+++ remotecv/bucket.py
@@ -0,0 +1,33 @@
+# Simple botocore Bucket loader implmentation
+
+
+import botocore.session
+
+class Bucket(object):
+	"""
+	Handles AWS S3 API
+	"""
+
+	_bucket = None
+	_region = None
+	_cache = dict()
+
+	def __init__(self, bucket, region):
+		"""
+		Constructor
+		:param string bucket: Bucket name
+		:param string region: AWS Region
+		:return: The initialized Bucket
+		"""
+		self._bucket = bucket
+		self._region = region
+		session = botocore.session.get_session()
+		self._client = session.create_client('s3', region_name=self._region)
+
+	def get(self, key):
+		"""
+		Returns objects matching key from bucket
+		:param string key: Key to retrieve the file
+		"""
+		s3_object = self._client.get_object(Bucket=self._bucket, Key=key)
+		return s3_object['Body'].read()
---
+++ remotecv/s3_loader.py
@@ -0,0 +1,20 @@
+# Simple implementation of thumbor-communities s3_storage
+
+
+from remotecv.utils import logger
+from bucket import bucket
+
+import os
+
+def load_sync(path):
+	"""
+	Loads image from S3 Bucket
+	:param string path: full path containing bucket
+	"""
+	bucket = path.lstrip('/').split('/')
+	key = '/'.join(path.lstrip('/')[1:])
+	region = os.environ.get('AWS_REGION', 'us-east-1')
+	
+	bucket_loader = Bucket(bucket, region)
+
+	return bucket_loader.get(key)
