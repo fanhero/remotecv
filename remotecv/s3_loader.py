c9d6b259c3785b13cd81318d2c094b52bae2e2dc
Christian Green <christian@fanhero.com>
2016-09-10 21:48:40 -0400

    Syntax fix

--- remotecv/s3_loader.py
+++ remotecv/s3_loader.py
@@ -13,7 +13,7 @@ def load_sync(path):
 	Loads image from S3 Bucket
 	:param string path: full path containing bucket
 	"""
-	path = urllib2.unquote(path)
+	path = str(path)
 	bucket = path.lstrip('/').split('/')
 	key = '/'.join(path.split('/'))[1:]
 	logger.warn('[PATCH_BUCKET_KEY]: ' + str(type(path)))
